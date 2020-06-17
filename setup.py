273947681d700dc3ff3a0bbdfad58ca173f1426e
Daniel Leicht <daniel.leicht@gmail.com>
2020-06-17 17:29:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
     
 setuptools.setup(
-    name="pylogix",
+    name="pylogix2",
     version="0.5.2",
     author="Daniel Leicht",
     author_email="daniel.leicht@gmail.com",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="Apache License 2.0",
-    url="https://github.com/daniel-leicht/pylogix",
+    url="https://github.com/daniel-leicht/pylogix2",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 2.7",
